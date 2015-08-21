4918a3d26c835401a7d0ce139624c7d390a2500c
The Gitter Badger <badger@gitter.im>
2015-08-21 16:50:01 -0700

    Add a Gitter chat badge to README.md
    Summary: ### facebook/wdt now has a Chat Room on Gitter
    
    @​ldemailly has just created a chat room. You can visit it here: [https://gitter.im/facebook/wdt](https://gitter.im/facebook/wdt?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&content=body_link).
    
    This pull-request adds this badge to your README.md:
    
    [![Gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/facebook/wdt?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=body_badge)
    
    If my aim is a little off, please [let me know](https://github.com/gitterHQ/readme-badger/issues).
    
    Happy chatting.
    
    PS: [Click here](https://gitter.im/settings/badger/opt-out) if you would prefer not to receive automatic pull-requests from Gitter in future.
    
    Closes https://github.com/facebook/wdt/pull/54
    
    Reviewed By: @​uddipta
    
    Differential Revision: D2372115
    
    Pulled By: @​ldemailly

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 `WDT` Warp speed Data Transfer
 ------------------------------
 
+[![Join the chat at https://gitter.im/facebook/wdt](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/facebook/wdt?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
 ## Design philosophy/Overview
 
 Goal:
--- WdtConfig.h
+++ WdtConfig.h
@@ -8,9 +8,9 @@
 
 #define WDT_VERSION_MAJOR 1
 #define WDT_VERSION_MINOR 16
-#define WDT_VERSION_BUILD 1508180
+#define WDT_VERSION_BUILD 1508210
 // Add -fbcode to version str
-#define WDT_VERSION_STR "1.16.1508180-fbcode"
+#define WDT_VERSION_STR "1.16.1508210-fbcode"
 // Tie minor and proto version
 #define WDT_PROTOCOL_VERSION WDT_VERSION_MINOR
 
