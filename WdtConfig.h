984ad2530d91e4db8d8f8253e19f5b452b0a26c8
Laurent Demailly <ldemailly@fb.com>
2015-09-29 21:51:25 -0700

    wdt bumping version
    Summary: I will try to ninja land this so no need to accept yet (unless I fail the ninja
    land)
    
    wdt bumping version
    
    Differential Revision: D2493165
    
    Reviewed By: @​svcscm

--- WdtConfig.h
+++ WdtConfig.h
@@ -8,9 +8,9 @@
 
 #define WDT_VERSION_MAJOR 1
 #define WDT_VERSION_MINOR 20
-#define WDT_VERSION_BUILD 1509290
+#define WDT_VERSION_BUILD 1509291
 // Add -fbcode to version str
-#define WDT_VERSION_STR "1.20.1509290-fbcode"
+#define WDT_VERSION_STR "1.20.1509291-fbcode"
 // Tie minor and proto version
 #define WDT_PROTOCOL_VERSION WDT_VERSION_MINOR
 
