46380a367e9f999d66c2472017c280b7d0cbce83
Laurent Demailly <ldemailly@fb.com>
2015-12-17 19:19:00 -0800

    more tests

--- build/travis_linux.sh
+++ build/travis_linux.sh
@@ -11,7 +11,7 @@ export PATH=$HOME/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
 openssl version
 which openssl
-ldd openssl
+ldd `which openssl`
 if [[ "$CXX" == "clang++" ]] ; then
   export CC=clang-3.6
   export CXX=clang++-3.6
