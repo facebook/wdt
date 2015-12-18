629418c13b13b4a8ef9818d0daa757c9c875559c
Laurent Demailly <ldemailly@fb.com>
2015-12-18 12:22:55 -0800

    Fix travis ssl
    Summary:
    by building openssl from source for now (pending newer packages availability)
    Closes https://github.com/facebook/wdt/pull/108
    
    Reviewed By: nikunjy
    
    Differential Revision: D2774328
    
    Pulled By: ldemailly
    
    fb-gh-sync-id: bfa9da690b4a076228a9a0aeaef01ea465f7b628

--- build/travis_linux.sh
+++ build/travis_linux.sh
@@ -9,6 +9,7 @@ echo $HOSTNAME
 mkdir $HOME/bin || true
 export PATH=$HOME/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
+openssl version
 if [[ "$CXX" == "clang++" ]] ; then
   export CC=clang-3.6
   export CXX=clang++-3.6
@@ -21,6 +22,15 @@ cd ..
 # https://github.com/travis-ci/travis-ci/issues/5059
 wget --no-check-certificate https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.sh
 sh cmake-3.3.2-Linux-x86_64.sh --prefix=$HOME --skip-license
+OPENSSL_VERSION=openssl-1.0.1q
+wget https://www.openssl.org/source/$OPENSSL_VERSION.tar.gz
+tar xfz $OPENSSL_VERSION.tar.gz
+ls -l $HOME/bin
+( cd $OPENSSL_VERSION ; ./config --prefix=$HOME threads shared; make ; make install )
+which openssl
+$HOME/bin/openssl version
+ldd $HOME/bin/openssl
+export OPENSSL_ROOT_DIR=$HOME
 git clone https://github.com/floitsch/double-conversion.git
 (cd double-conversion; cmake -DCMAKE_INSTALL_PREFIX=$HOME .; make -j 4 && make install)
 git clone https://github.com/schuhschuh/gflags.git
