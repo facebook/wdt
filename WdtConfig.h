e8422598eb9e2c74cd378ac053b63e420be7517f
Laurent Demailly <ldemailly@fb.com>
2015-12-22 21:03:33 -0800

    wdt version bump
    Summary: wdt version bump auto commit
    
    Reviewed By: ldemailly
    
    fb-gh-sync-id: ed95e389003ea88a60bc3b472558d79b04309046

--- WdtConfig.h
+++ WdtConfig.h
@@ -11,9 +11,9 @@
 
 #define WDT_VERSION_MAJOR 1
 #define WDT_VERSION_MINOR 24
-#define WDT_VERSION_BUILD 1512180
+#define WDT_VERSION_BUILD 1512220
 // Add -fbcode to version str
-#define WDT_VERSION_STR "1.24.1512180-fbcode"
+#define WDT_VERSION_STR "1.24.1512220-fbcode"
 // Tie minor and proto version
 #define WDT_PROTOCOL_VERSION WDT_VERSION_MINOR
 
