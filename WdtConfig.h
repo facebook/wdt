44a483832b5ea1e8968501d285671a6b78985e9d
svcscm@fb.com <svcscm@fb.com>
2016-01-25 15:59:52 -0800

    wdt version bump
    Summary: wdt version bump auto commit
    
    Reviewed By: svcscm
    
    fb-gh-sync-id: c64cfdf7d79c92926a9c266f278e94d59a4f8d94

--- WdtConfig.h
+++ WdtConfig.h
@@ -11,9 +11,9 @@
 
 #define WDT_VERSION_MAJOR 1
 #define WDT_VERSION_MINOR 25
-#define WDT_VERSION_BUILD 1601225
+#define WDT_VERSION_BUILD 1601250
 // Add -fbcode to version str
-#define WDT_VERSION_STR "1.25.1601225-fbcode"
+#define WDT_VERSION_STR "1.25.1601250-fbcode"
 // Tie minor and proto version
 #define WDT_PROTOCOL_VERSION WDT_VERSION_MINOR
 
