743fc668690882ca4f22878a099a21968ae51828
svcscm@fb.com <svcscm@fb.com>
2016-03-16 22:07:12 -0700

    wdt version bump
    Summary: wdt version bump auto commit
    
    fb-gh-sync-id: 359edb83ad1372826f14a4f9abddbf4dedffd46b
    shipit-source-id: 359edb83ad1372826f14a4f9abddbf4dedffd46b

--- WdtConfig.h
+++ WdtConfig.h
@@ -11,9 +11,9 @@
 
 #define WDT_VERSION_MAJOR 1
 #define WDT_VERSION_MINOR 26
-#define WDT_VERSION_BUILD 1603160
+#define WDT_VERSION_BUILD 1603161
 // Add -fbcode to version str
-#define WDT_VERSION_STR "1.26.1603160-fbcode"
+#define WDT_VERSION_STR "1.26.1603161-fbcode"
 // Tie minor and proto version
 #define WDT_PROTOCOL_VERSION WDT_VERSION_MINOR
 
