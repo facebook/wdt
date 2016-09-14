e985545272e7cd8c407aa1a0d2dfd501ef20e9a4
Uddipta Maity <uddipta@fb.com>
2016-09-13 17:54:36 -0700

    Reducing throttler rate in test so that it passes with asan
    Summary: Reducing throttler rate in test so that it passes with asan
    
    Reviewed By: ldemailly
    
    Differential Revision: D3861060
    
    fbshipit-source-id: f575fe636f90b4781fd1c3388cb31a5bd3c1a502

--- test/ThrottlerTest.cpp
+++ test/ThrottlerTest.cpp
@@ -11,33 +11,33 @@ namespace wdt {
 void testThrottling(std::shared_ptr<Throttler> throttler, int expectedRate) {
   int64_t numTransferred = 0;
   auto startTime = Clock::now();
-  while (durationSeconds(Clock::now() - startTime) < 2) {
+  while (durationSeconds(Clock::now() - startTime) <= 2) {
     throttler->limit(1000);
     numTransferred += 1000;
   }
   auto endTime = Clock::now();
   double durationSecs = durationSeconds(endTime - startTime);
   double throughputMBps = numTransferred / durationSecs / kMbToB;
-  EXPECT_NEAR(throughputMBps, expectedRate, 3);
+  EXPECT_NEAR(throughputMBps, expectedRate, 1);
 }
 
 TEST(ThrottlerTest, RATE_CHANGE) {
   WdtOptions options;
-  options.avg_mbytes_per_sec = 500;
+  options.avg_mbytes_per_sec = 50;
   std::shared_ptr<Throttler> throttler = Throttler::makeThrottler(options);
   throttler->startTransfer();
 
-  testThrottling(throttler, 500);
+  testThrottling(throttler, 50);
 
   // test rate decrease
-  options.avg_mbytes_per_sec = 300;
+  options.avg_mbytes_per_sec = 30;
   throttler->setThrottlerRates(options);
-  testThrottling(throttler, 300);
+  testThrottling(throttler, 30);
 
   // test rate increase
-  options.avg_mbytes_per_sec = 700;
+  options.avg_mbytes_per_sec = 70;
   throttler->setThrottlerRates(options);
-  testThrottling(throttler, 700);
+  testThrottling(throttler, 70);
 
   throttler->endTransfer();
 }
