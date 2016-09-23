b020553172d9d16ff808009bf1fb642be70faa5e
svcscm@fb.com <svcscm@fb.com>
2016-09-22 17:38:29 -0700

    wdt/stargate version bump
    Summary: wdt/stargate version bump auto commit
    
    fbshipit-source-id: a0dcea14a62ed196039be84e47c1e2adffd9ed3c

--- WdtConfig.h
+++ WdtConfig.h
@@ -11,9 +11,9 @@
 
 #define WDT_VERSION_MAJOR 1
 #define WDT_VERSION_MINOR 26
-#define WDT_VERSION_BUILD 1609210
+#define WDT_VERSION_BUILD 1609220
 // Add -fbcode to version str
-#define WDT_VERSION_STR "1.26.1609210-fbcode"
+#define WDT_VERSION_STR "1.26.1609220-fbcode"
 // Tie minor and proto version
 #define WDT_PROTOCOL_VERSION WDT_VERSION_MINOR
 
