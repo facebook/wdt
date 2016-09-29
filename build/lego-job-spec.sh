1887f2f5693c5cdaa765a81529e55b9b2cd53513
Laurent Demailly <ldemailly@fb.com>
2016-09-29 16:32:25 -0700

    legocastle json files for wdt build
    Summary:
    the _test file works:
    
    https://our.intern.facebook.com/intern/sandcastle/1971357929/
    
    Reviewed By: uddipta, perryjrandall
    
    Differential Revision: D3941714
    
    fbshipit-source-id: 5383d12e8c6ed291543683d042c7dd993ce23540

---
+++ build/lego-job-spec.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+cat <<EOF
+[{
+    "alias": "wdt-opensource",
+    "command": "SandcastleUniversalCommand",
+    "vcsType": "fbcode-fbsource",
+    "user": "ldemailly",
+    "capabilities": {
+        "vcs": "fbcode-fbsource",
+        "type": "lego-linux",
+        "os": "ubuntu_16.04"
+    },
+    "args": {
+        "name": "wdt-opensource",
+        "oncall": "wdt",
+        "timeout": 10800,
+        "steps": [
+            {
+                "user": "facebook",
+                "name": "Build Wdt",
+                "shell": "mkdir wdt_build; cd wdt_build; cmake ../wdt -DFOLLY_SOURCE_DIR=$(cd ..;pwd) -DBUILD_TESTING=on && make -j",
+                "required": true
+            },
+            {
+                "user": "facebook",
+                "name": "Run Wdt Tests",
+                "shell": "cd wdt_build; CTEST_OUTPUT_ON_FAILURE=1 make test",
+                "required": true
+            }
+        ],
+    }
+}]
+EOF
